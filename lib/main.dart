f31b5c6cead9c1a39b18f18c9f3f3afaa1cd784c
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2023-01-07 12:57:49 +0545

    Convert the Stateless widget to a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your favorite color?', 'What\'s your favorite animal?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('A Quiz App'),
         ),
-        body: const Center(
-          child: Text('This is a quiz app'),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                questions[questionIndex],
+              ),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: const Text('The Answer 1!'),
+              ),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: const Text('The Answer 2!'),
+              ),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: const Text('The Answer 3!'),
+              ),
+            ],
+          ),
         ),
       ),
     );
